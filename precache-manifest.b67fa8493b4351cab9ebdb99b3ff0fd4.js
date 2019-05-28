b23ba8c18ca2110c11fc7eca989050a6091c59d9
Duan <josiasduan@gmail.com>
2019-05-27 22:30:33 -0300

    calculator added

---
+++ precache-manifest.b67fa8493b4351cab9ebdb99b3ff0fd4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0716af17468f305421b3eea94d597eec",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "51a96c0f2ce398748a9e",
+    "url": "/myportfolio/static/css/main.c1fea73d.chunk.css"
+  },
+  {
+    "revision": "d2bae17cab67427c9727",
+    "url": "/myportfolio/static/js/2.5cca33b0.chunk.js"
+  },
+  {
+    "revision": "51a96c0f2ce398748a9e",
+    "url": "/myportfolio/static/js/main.dd388b6c.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "57de78e75e7d28a4c18dea3b998b668e",
+    "url": "/myportfolio/static/media/git.57de78e7.png"
+  },
+  {
+    "revision": "9249a518c53e99b3eb9c078855789475",
+    "url": "/myportfolio/static/media/header.9249a518.png"
+  },
+  {
+    "revision": "673cd9b612aa02a493ca0d752dc40099",
+    "url": "/myportfolio/static/media/insta.673cd9b6.png"
+  },
+  {
+    "revision": "d8dc03991b5843a9cf95b1f39427d2b9",
+    "url": "/myportfolio/static/media/profile.d8dc0399.png"
+  }
+]);
